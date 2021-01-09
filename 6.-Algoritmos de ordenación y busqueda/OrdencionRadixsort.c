a3a923e54f795d134df4550cd2e698afe71d176c
Saul Corona <saulcoronasantos@ymail.com>
2021-01-09 13:24:24 -0600

    Ordenacion Radixsort

---
+++ 6.-Algoritmos de ordenación y busqueda/OrdencionRadixsort.c
@@ -0,0 +1,92 @@
+/*Algoritmo de ordenación por metodo Radixsort*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define N 100
+void radix_sort(int a[], int n);
+void imprimirLista(int a[], int n);
+void entradaLista(int a[], int n);  
+
+
+
+int main() {
+ int n;
+ int v[N];
+ do{
+   printf("\nIntrodusca el numero de elementos: ");
+   scanf("%d", &n);
+ }while( (n<1) && (n>N));
+ entradaLista(v,n);
+  /*Muestra lista original*/
+  printf("\nLista original de %d elementos", n);
+  imprimirLista(v,n);
+  radix_sort(v,n);
+  printf("\nLista ordenada de %d elementos", n);
+  imprimirLista(v,n);
+  return 0;
+}
+
+int get_max (int a[], int n){
+   int max = a[0];
+   for (int i = 1; i < n; i++)
+      if (a[i] > max)
+         max = a[i];
+   return max;
+}
+
+void radix_sort(int a[], int n){
+   int bucket[10][10], bucket_cnt[10];
+   int i, j, k, r, NOP = 0, divisor = 1, lar, pass;
+   lar = get_max (a, n);
+   while (lar > 0){
+      NOP++;
+      lar /= 10;
+   }
+   for (pass = 0; pass < NOP; pass++){
+      for (i = 0; i < 10; i++){
+         bucket_cnt[i] = 0;
+      }
+      for (i = 0; i < n; i++){
+         r = (a[i] / divisor) % 10;
+         bucket[r][bucket_cnt[r]] = a[i];
+         bucket_cnt[r] += 1;
+      }
+      i = 0;
+      for (k = 0; k < 10; k++){
+         for (j = 0; j < bucket_cnt[k]; j++){
+            a[i] = bucket[k][j];
+            i++;
+         }
+      }
+      divisor *= 10;
+      printf ("\n Pasada %d : ", pass + 1);
+      for (i = 0; i < n; i++)
+         printf ("%d ", a[i]);
+      printf ("\n");
+   }
+}
+
+
+
+
+
+
+void imprimirLista(int a[], int n){
+  int i;
+  for (i =0; i < n; i++){
+    char c;
+    c = (i%10 == 0) ? '\n': ' ';
+    printf("%c %d", c, a[i]);
+  }
+}
+
+
+void entradaLista(int a[], int n){
+  int i;
+  printf("Entrada de los elementos\n");
+  for(i=0; i< n; i++){
+    printf("a[%d] = ", i+1);
+    scanf("%d", &a[i]);
+  }
+}
